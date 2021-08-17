9feb8b39c92426247a1dfae7086f33795f875bd4
Ruiwen Dong <dongrw426@gmail.com>
2021-08-17 14:21:28 -0400

    react-redux-form validation2
    includes: react-redux-form, errors component validation, and Modal object

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import React, {Component} from 'react';
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button,
+    Modal, ModalHeader, ModalBody, Label, Row, Col} from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 function RenderDish({dish}){
         if(dish != null)
@@ -27,7 +28,7 @@ function RenderDish({dish}){
             return(
                 <li key={comment.id}>
                     {comment.comment}
-                    <br /><br />
+                    <br />
                     -- {comment.author}, 
                     {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                     <br /><br />
@@ -39,6 +40,7 @@ function RenderDish({dish}){
              <div>
                  <h4>Comments</h4>
                  <ul className="list-unstyled">{commentList}</ul>
+                 <CommentForm />
              </div>
          )
        }
@@ -49,6 +51,7 @@ function RenderDish({dish}){
        }
        
     }
+
     const DishDetail = (props) => {
         if(props.dish != null){
             return (
@@ -80,3 +83,101 @@ function RenderDish({dish}){
         }
     }
     export default DishDetail;
+
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+    const minLength = (len) => (val) => val && (val.length >= len);
+
+    export class CommentForm extends Component {
+
+        constructor(props){
+            super(props)
+
+            this.state = {
+                isModalOpen: false
+            }
+        
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        }
+
+        toggleModal(){
+            this.setState({
+                isModalOpen: !this.state.isModalOpen
+            })
+        }
+
+        handleSubmit(values){
+            console.log("Current State is:" + JSON.stringify(values));
+            alert("Current State is:" + JSON.stringify(values));
+        }
+
+        render() {
+                return(
+                    <div>
+                        <Button color="secondary" onClick={this.toggleModal}>
+                        <i className="fa fa-pencil"></i>
+                        Submit Comment
+                    </Button>
+                    <div className="row row-content">
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Content</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group"> 
+                                <Label className="ml-3" htmlfor="rating">Rating</Label>
+                                <Col>
+                                    <Control.select model=".rating" name="rating"
+                                        className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                            <Label className="ml-3" htmlFor="telnum">Your Name</Label>
+                                <Col >
+                                    <Control.text model=".yourname" id="yourname" name="yourname"
+                                        placeholder="Your Name" 
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} />
+                                        <Errors 
+                                            className="text-danger"
+                                            model=".yourname"
+                                            show="touched"
+                                            messages={{
+                                                required: 'Required',
+                                                minLength: 'Must be greater than 2 characters',
+                                                maxLength: 'Must be 15 characters or less',
+                                            }}
+                                        />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label className="ml-3" htmlFor="comment">Comment</Label>
+                                <Col>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        rows="12" className="form-control"/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Button type="submit" color="primary">Submit</Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+                    </div>
+                    </div>
+                    
+                    
+                )
+        }
+    }
+    
